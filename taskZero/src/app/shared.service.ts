85b94040a33a9e6077847c8dcdd727d04ab8a2c6
Santhosh Selvaraj <santhoshselvaraj85@gmail.com>
2021-01-31 22:29:48 -0600

    Initial commit

---
+++ taskZero/src/app/shared.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  constructor() { }
+  private signUpData:any;
+  
+  private students = [];
+
+  AddSignUpData(signUpData:any):void
+  {
+    this.signUpData = signUpData;
+  }
+  getSignUpData():any{
+    return this.signUpData;
+  }
+
+  // getSignUpPage():boolean{
+  //   return this.signUpPage;
+  // }
+
+  // setSignUpPage(flag:boolean):void{
+  //   this.signUpPage = flag;
+  // }
+
+}
